30c81d96ec282abcf35937d9b93e813f5c32ce20
Illia Byndas <50154894+Leynwell@users.noreply.github.com>
2024-06-04 16:53:51 +0300

    task functions

--- internal/infra/http/router.go
+++ internal/infra/http/router.go
@@ -3,15 +3,16 @@ package http
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/BohdanBoriak/boilerplate-go-back/config"
 	"github.com/BohdanBoriak/boilerplate-go-back/config/container"
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/controllers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -49,6 +50,7 @@ func Router(cont container.Container) http.Handler {
 				apiRouter.Use(cont.AuthMw)
 
 				UserRouter(apiRouter, cont.UserController)
+				TaskRouter(apiRouter, cont.TaskController)
 				apiRouter.Handle("/*", NotFoundJSON())
 			})
 		})
@@ -97,6 +99,34 @@ func UserRouter(r chi.Router, uc controllers.UserController) {
 			"/",
 			uc.Delete(),
 		)
+	})
+}
+
+func TaskRouter(r chi.Router, tc controllers.TaskController) {
+	r.Route("/tasks", func(apiRouter chi.Router) {
+		apiRouter.Post(
+			"/",
+			tc.Save(),
+		)
+		apiRouter.Get(
+			"/",
+			tc.GetForUser(),
+		)
+
+		// EXAM
+		apiRouter.Get(
+			"/{id}",
+			tc.FindById(),
+		)
+		apiRouter.Put(
+			"/",
+			tc.Update(),
+		)
+		apiRouter.Delete(
+			"/{id}",
+			tc.Delete(),
+		)
+		// EXAM END
 	})
 }
 
